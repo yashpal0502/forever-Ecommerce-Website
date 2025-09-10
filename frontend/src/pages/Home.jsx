6854a6dc96dcd00a9a3897a1f4209d351a6e063b
yashpal0502 <cyash0299@gmail.com>
2025-09-11 03:53:15 +0530

    My first Web-Dev Project

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import BestSeller from "../components/BestSeller";
 import OurPolicy from "../components/OurPolicy";
-import NewsLetterBox from "../components/NewsLetterBox";
+import NewsLetterBox from "../components/NewsletterBox";
 
 const Home = () => {
   return (
