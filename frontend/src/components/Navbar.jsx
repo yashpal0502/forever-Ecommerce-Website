8654310291065cd6a73c5048284e389615e64803
yashpal0502 <cyash0299@gmail.com>
2025-09-18 23:26:34 +0530

    Profile icon logic updated in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const {
     setShowSearch,
     getCartCount,
@@ -61,24 +62,35 @@ const Navbar = () => {
         <div className="group relative">
           {/* <Link to="/login"> */}
           <img
-            onClick={() => (token ? null : navigate("/login"))}
+            onClick={() =>
+              token ? setDropdownOpen((prev) => !prev) : navigate("/login")
+            }
             className="w-5 cursor-pointer"
             src={assets.profile_icon}
             alt=""
           />
           {/* </Link> */}
           {/* Dropdown */}
-          {token && (
-            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+          {token && dropdownOpen && (
+            <div className="absolute dropdown-menu right-0 pt-4">
               <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                 <p className="cursor-pointer hover:text-black">My Profile</p>
                 <p
-                  onClick={() => navigate("/orders")}
+                  onClick={() => {
+                    setDropdownOpen(false);
+                    navigate("/orders");
+                  }}
                   className="cursor-pointer hover:text-black"
                 >
                   Orders
                 </p>
-                <p onClick={logout} className="cursor-pointer hover:text-black">
+                <p
+                  onClick={() => {
+                    setDropdownOpen(false);
+                    logout();
+                  }}
+                  className="cursor-pointer hover:text-black"
+                >
                   Logout
                 </p>
               </div>
