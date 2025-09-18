77cea2e30edee450cec3b00c80e084b24a10b8fc
yashpal0502 <cyash0299@gmail.com>
2025-09-18 23:12:32 +0530

    currency symbol updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
-export const currency = "$";
+export const currency = "₹";
 
 const App = () => {
   const [token, setToken] = useState(
